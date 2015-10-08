fc8663a0c9061e691422d4a38da360735540607d
mckshreder <garrettmckenzie@ymail.com>
2015-10-08 15:35:22 -0700

    applying changes to server.js in order to link routes to angular application

--- server.js
+++ server.js
@@ -12,14 +12,18 @@ var port = process.env.PORT || 8080; // set the port for our app
 // APP CONFIGURATION ---------------------
 var jwt = require('jsonwebtoken'); //grab the jsonwebtoken package
 var config = require('./config');
+// API ROUTES ------------------------
+var apiRoutes = require('./app/routes/api')(app, express);
+app.use('/api', apiRoutes);
 
+// log all requests to the console
+app.use(morgan('dev'));
  // connect to our database (hosted on modulus.io)
 mongoose.connect(config.database);
 
 // use body parser so we can grab information from POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 // configure our app to handle CORS requests
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,203 +32,202 @@ app.use(function(req, res, next) {
 	next();
 });
 
-// log all requests to the console
-app.use(morgan('dev'));
-
-// basic route for the home page
-app.get('/', function(req, res) {
-	res.send('Welcome to the home page!');
-});
+// MAIN CATCHALL ROUTE ---------------
+// SEND USERS TO FRONTEND ------------
+// has to be registered after API ROUTES
+ app.get('*', function(req, res) {
+ res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+ });
 
 // =============================
 // ROUTES FOR OUR API
 
 
-// get an instance of the express router
-var apiRouter = express.Router();
+// // get an instance of the express router
+// var apiRouter = express.Router();
 
-// route for authenticating users
-apiRouter.post('/authenticate', function(req, res) {
+// // route for authenticating users
+// apiRouter.post('/authenticate', function(req, res) {
 
-	// find the user
-	// select the name username and password explicitly
-	User.findOne({
-	username: req.body.username
-	}).select('name username password').exec(function(err, user) {
+// 	// find the user
+// 	// select the name username and password explicitly
+// 	User.findOne({
+// 	username: req.body.username
+// 	}).select('name username password').exec(function(err, user) {
 
-		if (err) throw err;
+// 		if (err) throw err;
 
-		// no user with that username was found
-		if (!user) {
-			res.json({
-			success: false,
-			message: 'Authentication failed. User not found.'
+// 		// no user with that username was found
+// 		if (!user) {
+// 			res.json({
+// 			success: false,
+// 			message: 'Authentication failed. User not found.'
 
-			});
-		} else if (user) {
+// 			});
+// 		} else if (user) {
 
-		// check if password matches
-		var validPassword = user.comparePassword(req.body.password);
-		if (!validPassword) {
-			res.json({
-			success: false,
-			message: 'Authentication failed. Wrong password.'
-			});
-		} else {
-		// if user is found and password is right
-		// create a token
-		var token = jwt.sign({
-			name: user.name,
-			username: user.username
-		}, superSecret, {
-			expiresInMinutes: 1440 // expires in 24 hours
-		});
+// 		// check if password matches
+// 		var validPassword = user.comparePassword(req.body.password);
+// 		if (!validPassword) {
+// 			res.json({
+// 			success: false,
+// 			message: 'Authentication failed. Wrong password.'
+// 			});
+// 		} else {
+// 		// if user is found and password is right
+// 		// create a token
+// 		var token = jwt.sign({
+// 			name: user.name,
+// 			username: user.username
+// 		}, superSecret, {
+// 			expiresInMinutes: 1440 // expires in 24 hours
+// 		});
 
-		// return the information including token as JSON
-		res.json({
-			success: true,
-			message: 'Enjoy your token!',
-			token: token
-		});
-		}
-		}
+// 		// return the information including token as JSON
+// 		res.json({
+// 			success: true,
+// 			message: 'Enjoy your token!',
+// 			token: token
+// 		});
+// 		}
+// 		}
 
-	});
-});
+// 	});
+// });
 
-// middleware to use for all requests
-apiRouter.use(function(req, res, next) {
-// do logging
-	console.log('Somebody just came to our app!');
-	// check header or url parameters or post parameters for token
-	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+// // middleware to use for all requests
+// apiRouter.use(function(req, res, next) {
+// // do logging
+// 	console.log('Somebody just came to our app!');
+// 	// check header or url parameters or post parameters for token
+// 	var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-	// decode token
-	if (token) {
+// 	// decode token
+// 	if (token) {
 
-		// verifies secret and checks exp
-		jwt.verify(token, superSecret, function(err, decoded) {
-			if (err) {
-				return res.status(403).send({
-				success: false,
-				message: 'Failed to authenticate token.'
-			});
+// 		// verifies secret and checks exp
+// 		jwt.verify(token, superSecret, function(err, decoded) {
+// 			if (err) {
+// 				return res.status(403).send({
+// 				success: false,
+// 				message: 'Failed to authenticate token.'
+// 			});
 				
-			} else {
-				// if everything is good, save to request for use in other routes
-				req.decoded = decoded;
+// 			} else {
+// 				// if everything is good, save to request for use in other routes
+// 				req.decoded = decoded;
 
-				next();
+// 				next();
 
-			}
-		});
+// 			}
+// 		});
 
-	} else {
+// 	} else {
 
-	// if there is no token
-	// return an HTTP response of 403 (access forbidden) and an error message
-	return res.status(403).send({
-	success: false,
-	message: 'No token provided.'
-	});
+// 	// if there is no token
+// 	// return an HTTP response of 403 (access forbidden) and an error message
+// 	return res.status(403).send({
+// 	success: false,
+// 	message: 'No token provided.'
+// 	});
 
-	}
+// 	}
 
 
-});
-// test route to make sure everything is working
-// accessed at GET http://localhost:8080/api
-apiRouter.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
-});
+// });
+// // test route to make sure everything is working
+// // accessed at GET http://localhost:8080/api
+// apiRouter.get('/', function(req, res) {
+// 	res.json({ message: 'hooray! welcome to our api!' });
+// });
 
-// =============================
-// on routes that end in /users
-apiRouter.route('/users')
+// // =============================
+// // on routes that end in /users
+// apiRouter.route('/users')
 
-	.post(function(req, res) {
+// 	.post(function(req, res) {
 
-		// create a new instance of the User model
-		var user = new User();
+// 		// create a new instance of the User model
+// 		var user = new User();
 
-		// set the users information (comes from the request)
-		user.name = req.body.name;
-		user.username = req.body.username;
-		user.password = req.body.password;
+// 		// set the users information (comes from the request)
+// 		user.name = req.body.name;
+// 		user.username = req.body.username;
+// 		user.password = req.body.password;
 
-		// save the user and check for errors
-		user.save(function(err) {
-		if (err) {
-			// duplicate entry
-			 if (err.code == 11000)
-			 return res.json({ success: false, message: 'A user with that username already exists.' });
-			else
-			return res.send(err);
-			}
-			res.json({ message: 'User created!' });
-		});
-	})
-	// get all the users (accessed at GET http://localhost:8080/api/users)
-	.get(function(req, res) {
+// 		// save the user and check for errors
+// 		user.save(function(err) {
+// 		if (err) {
+// 			// duplicate entry
+// 			 if (err.code == 11000)
+// 			 return res.json({ success: false, message: 'A user with that username already exists.' });
+// 			else
+// 			return res.send(err);
+// 			}
+// 			res.json({ message: 'User created!' });
+// 		});
+// 	})
+// 	// get all the users (accessed at GET http://localhost:8080/api/users)
+// 	.get(function(req, res) {
 	 
-		User.find(function(err, users) {
-			if (err) res.send(err);
-			// return the users
-			res.json(users);
-		});
-	});
+// 		User.find(function(err, users) {
+// 			if (err) res.send(err);
+// 			// return the users
+// 			res.json(users);
+// 		});
+// 	});
 
-//----------------------------------------------------
-// on routes that end in /users/:user_id
-apiRouter.route('/users/:user_id')
+// //----------------------------------------------------
+// // on routes that end in /users/:user_id
+// apiRouter.route('/users/:user_id')
 
-	// get the user with that id
-	// (accessed at GET http://localhost:8080/api/users/:user_id)
-	.get(function(req, res) {
+// 	// get the user with that id
+// 	// (accessed at GET http://localhost:8080/api/users/:user_id)
+// 	.get(function(req, res) {
 
-		User.findById(req.params.user_id, function(err, user){
-			if (err) res.send(err);
-		// return that user
-		res.json(user);
-		});
-	})
+// 		User.findById(req.params.user_id, function(err, user){
+// 			if (err) res.send(err);
+// 		// return that user
+// 		res.json(user);
+// 		});
+// 	})
 
-	// update the user with this id
-	// (accessed at PUT http://localhost:8080/api/users/:user_id)
-	.put(function(req, res) {
+// 	// update the user with this id
+// 	// (accessed at PUT http://localhost:8080/api/users/:user_id)
+// 	.put(function(req, res) {
 
-		// use our user model to find the user we want
-		User.findById(req.params.user_id, function(err, user) {
-			if (err) res.send(err);
-			// update the users info only if its new
-			if (req.body.name) user.name = req.body.name;
-			if (req.body.username) user.username = req.body.username;
-			if (req.body.password) user.password = req.body.password;
-			// save the user
-			user.save(function(err) {
-				if (err) res.send(err);
-				// return a message
-				res.json({ message: 'User updated!' });
-			});
-		});
-	})
+// 		// use our user model to find the user we want
+// 		User.findById(req.params.user_id, function(err, user) {
+// 			if (err) res.send(err);
+// 			// update the users info only if its new
+// 			if (req.body.name) user.name = req.body.name;
+// 			if (req.body.username) user.username = req.body.username;
+// 			if (req.body.password) user.password = req.body.password;
+// 			// save the user
+// 			user.save(function(err) {
+// 				if (err) res.send(err);
+// 				// return a message
+// 				res.json({ message: 'User updated!' });
+// 			});
+// 		});
+// 	})
 
-	// (accessed at DELETE http://localhost:8080/api/users/:user_id)
-	.delete(function(req, res) {
-	// delete the user with this id
-		User.remove({
-			_id: req.params.user_id
-		}, 
-		function(err, user) {
-			if (err) return res.send(err);
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
+// 	// (accessed at DELETE http://localhost:8080/api/users/:user_id)
+// 	.delete(function(req, res) {
+// 	// delete the user with this id
+// 		User.remove({
+// 			_id: req.params.user_id
+// 		}, 
+// 		function(err, user) {
+// 			if (err) return res.send(err);
+// 			res.json({ message: 'Successfully deleted' });
+// 		});
+// 	});
 
 //	----------------------------------------------------
 // REGISTER OUR ROUTES
 // all of our routes will be prefixed with /api
-app.use('/api', apiRouter);
+app.use('/api', apiRoutes);
 
 // START THE SERVER
 // ===============================
